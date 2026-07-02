9952da7ce73515e5d6e5ff563b683d9bbfc95965
muhammed sahal <muhammedsahalap717@gmail.com>
2026-07-02 20:53:54 +0530

    refactor: destructure props in QuoteDisplay component for improved readability

--- src/QuoteDisplay.jsx
+++ src/QuoteDisplay.jsx
@@ -1,16 +1,16 @@
 import { Check, Copy } from "lucide-react";
 
 // This child component accepts a "props" object from the parent
-function QuoteDisplay(props) {
+function QuoteDisplay({ text, author, copiedStatus, onCopy }) {
   return (
     <div>
       {/* We read values out of the props object */}
-      <p className="quote-text">"{props.text}"</p>
-      {props.author && <span className="quote-author">~ {props.author}</span>}
+      <p className="quote-text">"{text}"</p>
+      {author && <span className="quote-author">~ {author}</span>}
 
       {/* We can even pass down functions like click handlers through props! */}
-      <button className="copy-btn" onClick={props.onCopy}>
-        {props.copiedStatus ? (
+      <button className="copy-btn" onClick={onCopy}>
+        {copiedStatus ? (
           <Check className="icon-green" size={20} />
         ) : (
           <Copy className="icon-gray" size={20} />
